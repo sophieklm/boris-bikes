14eabe9211d4ce6dfb1a479029b247c2af06bdbd
Sophie Mann <sklm@live.co.uk>
2017-04-11 18:07:47 +0100

    add instance variables

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,4 +8,18 @@ describe DockingStation do
     expect(bike).to be_instance_of(Bike)
     expect(bike).to be_working
   end
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
