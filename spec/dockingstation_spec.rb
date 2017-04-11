0f952d1d3038323d02eb26b51902e3394aa19ecd
Sophie Mann <sklm@live.co.uk>
2017-04-11 14:51:50 +0100

    update dockingstation tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,6 +5,7 @@ describe DockingStation do
   it {is_expected.to respond_to :release_bike}
   it 'releases a bike that works' do
     bike = subject.release_bike
+    expect(bike).to be_instance_of(Bike)
     expect(bike).to be_working
   end
 end
