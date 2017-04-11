15ece55aa55bafe56f46546f7710ff43157c86ce
Sophie Mann <sklm@live.co.uk>
2017-04-11 14:05:53 +0100

    update release_bike and working methods

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,10 @@
 require 'dockingstation'
+require 'bike'
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike }
+  it {is_expected.to respond_to :release_bike}
+  it 'releases a bike that works' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
