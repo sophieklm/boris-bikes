8cfa3ec64da66ca31ee70b498977c1a9b7b9bec5
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-12 17:28:47 +0100

    refactored dockingstation_spec to read better

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,33 +3,36 @@ require 'bike'
 
 describe DockingStation do
   bike = Bike.new
-  it {is_expected.to respond_to :release_bike}
-  it {is_expected.to respond_to(:dock).with(1).argument}
 
-  it 'releases a bike that works' do
-    expect(bike).to be_instance_of(Bike)
-    expect(bike).to be_working
+  describe '#dock' do
+    it {is_expected.to respond_to(:dock).with(1).argument}
+    it 'docks a bike' do
+      expect(subject.dock(bike)).to eq [bike]
+    end
+    it 'returns a bike that has been docked' do
+      subject.dock(bike)
+      expect(subject.bikes).to eq [bike]
+    end
+    it 'raises an error when dockingstation is full' do
+      (subject.capacity).times { subject.dock(bike) }
+      expect{subject.dock(bike)}.to raise_error(RuntimeError)
+    end
   end
 
-  it 'docks a bike' do
-    expect(subject.dock(bike)).to eq [bike]
+  describe '#release_bike' do
+    it {is_expected.to respond_to :release_bike}
+    it 'raises an error when no bikes' do
+      expect{subject.release_bike}.to raise_error(RuntimeError)
+    end
+    it 'releases a bike that works' do
+      expect(bike).to be_instance_of(Bike)
+      expect(bike).to be_working
+    end
   end
 
-  it 'returns a bike that has been docked' do
-    subject.dock(bike)
-    expect(subject.bikes).to eq [bike]
-  end
-
-  it 'raises an error when no bikes' do
-    expect{subject.release_bike}.to raise_error(RuntimeError)
-  end
-
-  it 'raises an error when dockingstation is full' do
-    (subject.capacity).times { subject.dock(bike) }
-    expect{subject.dock(bike)}.to raise_error(RuntimeError)
-  end
-
-  it 'sets capacity' do
-    expect(subject.capacity).to be DockingStation::DEFAULT_CAPACITY
-  end
+  describe '#initialize' do
+    it 'sets capacity' do
+      expect(subject.capacity).to be DockingStation::DEFAULT_CAPACITY
+    end
+  end  
 end
