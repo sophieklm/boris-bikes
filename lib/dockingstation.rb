408515c07c0e77224a6a26db6b42a2e2298fe662
Sophie Mann <sklm@live.co.uk>
2017-04-14 12:41:18 +0100

    update release_bike method

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -15,11 +15,7 @@ class DockingStation
   def release_bike
     raise RuntimeError, "There are no bikes" if empty?
     working_bikes = []
-    @bikes.each_with_index {|bike, i|
-      unless bike.broken?
-        working_bikes << i
-      end
-    }
+    @bikes.each_with_index {|bike, i| working_bikes << i unless bike.broken?}
     raise RuntimeError, "There are no working bikes" if working_bikes.count == 0
     output = @bikes.fetch(working_bikes[0])
     @bikes.delete_at(working_bikes[0])
